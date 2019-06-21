e27602ef8bcecf4e261764494b63dfd39c948243
Admin <Admin@DESKTOP-DJRATBL>
2019-06-21 17:12:43 +0530

    verify login 1 page
    uploading

---
+++ POM1/src/login1page/verifylogn1.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package login1page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import pages1.loginpage1storing;
+
+/**
+ * @author Admin
+ *
+ */
+public class verifylogn1 {
+	
+	@Test
+	public void verfylogin() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.topexamer.com/login");
+		driver.manage().window().maximize();
+		
+		loginpage1storing login = new loginpage1storing(driver);
+		
+		/*
+		login.TypePHONENUMBER("7893655956");
+		login.TypePASSWORD("1234567");
+		login.Clickonlogin();
+		*/
+		
+		login.login("7893655956", "123456");
+	
+		driver.close();
+		
+		
+		
+	}
+
+}
